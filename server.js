eb4a49423c2ff7dd687ba26f6d5a1a210a95eddf
Eminence <mulemanowa@gmail.com>
2021-10-16 23:14:32 +0200

    dependances installation

---
+++ server.js
@@ -0,0 +1,18 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+app.use(cookieParser());
+
+// Routes
+// app.use("/user", require("./routes/userRouter"));
+// app.use("/api", require("./routes/upload"));
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log(`Le server tourne sur le port : ${PORT}`);
+});
