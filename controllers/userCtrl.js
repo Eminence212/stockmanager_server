27fd2ca9e4996355e0ec42313d4dbe6de28ad515
Eminence <mulemanowa@gmail.com>
2021-10-28 13:16:42 +0100

    login controller fixed

--- controllers/userCtrl.js
+++ controllers/userCtrl.js
@@ -79,7 +79,7 @@ const userCtrl = {
       const refresh_token = createRefreshToken({ id: user.id });
       res.cookie("refreshtoken", refresh_token, {
         httpOnly: true,
-        path: "https://stockmanager-app.herokuapp.com/user/refresh_token",
+        path: "/user/refresh_token",
         maxAge: 7 * 24 * 60 * 60 * 1000, //7 days
         
       });
