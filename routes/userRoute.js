44f731c71159e1c7e52f197ded215ee577d2c505
Eminence <mulemanowa@gmail.com>
2021-11-03 01:17:41 +0100

    reset passwor route fixed

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -8,7 +8,7 @@ router.post("/activation", userCtrl.activateAccount);
 router.post("/login", userCtrl.login);
 router.post("/refresh_token", userCtrl.getAccessToken);
 router.post("/forgot", userCtrl.forgotPassword);
-router.post("/reset", auth, userCtrl.resetPassword);
+router.post("/reset", userCtrl.resetPassword);
 router.get("/infor", auth, userCtrl.getUserInfor);
 router.get("/all_infor", auth, authAdmin, userCtrl.getUsersAllInfor);
 router.get("/logout", userCtrl.logout);
