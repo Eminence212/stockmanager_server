32f6438d26a5188e6ef9c62f41eacbeab6c10550
Eminence <eminence@kinshasadigital.com>
2021-12-23 14:40:42 +0100

    fix update user

--- controllers/userCtrl.js
+++ controllers/userCtrl.js
@@ -176,13 +176,13 @@ const userCtrl = {
   updateUser: async (req, res) => {
     try {
       const id = req.params.id;
-      const { name } = req.body;
+      const { name,role } = req.body;
       const userById = await Users.findOne({ where: { id: id } });
       if (!userById) {
         return res.status(404).json({ msg: "Non trouvé" });
       }
       const userToUpdate = await Users.findOne({ where: { name } });
-      if (!userToUpdate) await Users.update({ name }, { where: { id: id } });
+      if (!userToUpdate) await Users.update({ name,role }, { where: { id: id } });
       res.json({ msg: "Mise à jour effectuée avec succès !" });
     } catch (error) {
       return res.status(500).json({ msg: error.message });
