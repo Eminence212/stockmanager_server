9b23feeba503a266c4c99b2b5a0991f83ee82566
Eminence <eminence@kinshasadigital.com>
2021-12-30 17:29:15 +0100

    fix response after customer created

--- controllers/customerCtrl.js
+++ controllers/customerCtrl.js
@@ -31,10 +31,10 @@ const customerCtrl = {
         });
 
       const newCustomer = { name, contact };
-      await Customers.create(newCustomer);
-      res.json({ msg: "Client ajouté avec succès !" });
+      const customer=await Customers.create(newCustomer);
+      res.json({ msg: "Client ajouté avec succès !", customer });
     } catch (error) {
-      return res.status(500).json({ msg: error.message });
+      return res.status(500).json({ msg: error.message});
     }
   },
 
