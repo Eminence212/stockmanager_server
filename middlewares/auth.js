e07179ab5f91dc3d138fcf094b6388a00940e8b3
Eminence <eminence@caravanserail.co>
2022-04-07 16:11:28 +0100

    fixed jwt verify error message

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,19 +1,23 @@
-const jwt = require("jsonwebtoken");
+const jwt = require('jsonwebtoken');
 
 const auth = (req, res, next) => {
   try {
-    const token = req.header("Authorization")
-    if (!token) return res.status(400).json({ msg: "Authentification invalide" });
-    
+    const token = req.header('Authorization');
+    if (!token)
+      return res
+        .status(400)
+        .json({ msg: 'Authentification invalide, token vide.' });
+
     jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
-      if (err) return res.status(400).json({ msg: "Authentification invalide" });
-      req.user = user
-      next()
+      if (err)
+        return res
+          .status(400)
+          .json({ msg: 'Authentification invalide pour le jwt verify' });
+      req.user = user;
+      next();
     });
-    
   } catch (error) {
-     return res.status(500).json({ msg: error.message });
-    
+    return res.status(500).json({ msg: error.message });
   }
 };
 module.exports = auth;
