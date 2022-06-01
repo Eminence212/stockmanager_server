944c31a54c59d031dfecf75fbd75d8960fc34419
Eminence <eminence@caravanserail.co>
2022-06-01 12:06:27 +0100

    addeed seeders

--- seeders/20220601085403-user.js
+++ seeders/20220601085403-user.js
@@ -1,13 +1,14 @@
 ('use strict');
 const bcrypt = require('bcrypt');
 require('dotenv').config();
-const { USER, PASSWORD } = process.env;
+const { USER, PASSWORD, ROLE } = process.env;
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     return queryInterface.bulkInsert('Users', [
       {
         name: USER,
         password: await bcrypt.hash(PASSWORD, 12),
+        role: ROLE,
         createdAt: new Date(),
         updatedAt: new Date(),
       },
